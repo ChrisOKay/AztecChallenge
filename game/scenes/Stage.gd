52c0846bf89156c15ae430a99d784230f6f20cb2
Thomas Karcher <t_karcher@posteo.de>
2016-10-30 23:30:09 +0100

    Level creation outsourced in own class for easier addition of new levels.

--- game/scenes/Stage.gd
+++ game/scenes/Stage.gd
@@ -1,6 +1,7 @@
 
 extends Node2D
 
+const TILE_SIZE = 40 # Don't change this without resizing all images accordingly
 const JUMP_SPEED = 280 # Vertical speed used both for upward and downward movement
 const WALK_SPEED = 80 # Horizonal scrolling speed
 const JUMP_VALUES = \
@@ -10,16 +11,11 @@ const JUMP_VALUES = \
 		{"height":120, "sound":"jump2", "points":50}, \
 	"HIGH": \
 		{"height":180, "sound":"jump3", "points":100}}
-const MAX_HEIGHT = 140 # maximum height of wall columns
-const V_OFFSET = 335 # Vertical offset of wall columns on Stage
-const BUMPINESS = 0.3 # From 0 (completely smooth) to 1 (extremely bumpy);
-const FERTILITY = 0.25 # From 0 (no cactuses at all) to 1 (thick forest)
-const TILE_SIZE = 40 # Don't change this without resizing all images accordingly
 
-var scnPhaseTitle = preload ("res://scenes/PhaseTitle.tscn")
-var scnWall = preload ("res://scenes/Wall.tscn")
-var scnCactus = preload ("res://scenes/Cactus.tscn")
-var curGameNode = Node.new() # temporary node to quickly delete all subnodes
+const LEVEL = { \
+	"Phase 1": preload ("res://scenes/Level_1.gd")}
+
+var levels = [] # array of created levels
 var players = [] # array of active players
 var intHighscore = 0
 
@@ -146,42 +142,14 @@ func initGame():
 	get_viewport().set_canvas_transform(Matrix32(0,Vector2(0,0)))
 	
 	# delete previously created landscape
-	curGameNode.free()
-	
+	for l in levels: l.free()
+	levels.clear()
+
 	# create new landscape
-	curGameNode = Node.new()
-	add_child(curGameNode)
+	for l in LEVEL.values(): levels.append(l.new())
+	for n in levels: add_child(n)
 	
-	# ====== Phase 1 "The Grounds" =====
-	#    (Length: 20 + 60 Tiles)
 	
-	# ------ Tiles 0-20: Title only ------
-	var ndPhaseTitle = scnPhaseTitle.instance()
-	ndPhaseTitle.set_text("The Grounds")
-	ndPhaseTitle.set_pos(Vector2(400,300))
-	curGameNode.add_child(ndPhaseTitle)
-	
-	# ------ Tiles 21-60: Landscape without platforms ------
-	# set initial wall height
-	var h = floor(rand_range(0,MAX_HEIGHT)/TILE_SIZE)
-	
-	for i in range(20,80):
-
-		# create new wall column, set location and add to Stage
-		var ndWall = scnWall.instance()
-		ndWall.translate(Vector2(i*TILE_SIZE,V_OFFSET+(h*TILE_SIZE)))
-		curGameNode.add_child (ndWall)
-
-		# add cactuses every so often
-		if randf() < FERTILITY:
-			var ndCactus = scnCactus.instance()
-			ndCactus.translate(Vector2(i*TILE_SIZE, V_OFFSET+(h*TILE_SIZE)))
-			curGameNode.add_child(ndCactus)
-			
-		# change wall height every so often
-		if randf() < BUMPINESS:
-			h = floor(rand_range(0,MAX_HEIGHT)/TILE_SIZE)
-		
 	# listen to key events
 	set_process_input(true)
 
